f49e67590d0e57cc65d3be1ed727d95976d65cff
weilee <19042049@cnsuning.com>
2019-11-25 20:18:05 +0800

    add pricewarning example

---
+++ examples/pricewarning/www/precache-manifest.c1cdaf56f552f5676374da0d19f51c53.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d13c5411c1cb46a786e3",
+    "url": "/assets/css/app.d45cf583.css"
+  },
+  {
+    "revision": "d13c5411c1cb46a786e3",
+    "url": "/assets/js/app.8d569a59.js"
+  },
+  {
+    "revision": "6ea1bc3d16cbeafe4ea3",
+    "url": "/assets/css/chunk-vendors.41efaf0e.css"
+  },
+  {
+    "revision": "6ea1bc3d16cbeafe4ea3",
+    "url": "/assets/js/chunk-vendors.cb307b5d.js"
+  },
+  {
+    "revision": "143146fa24554ae2c5ac0a3982abb952",
+    "url": "/assets/fonts/ionicons.143146fa.woff2"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "/assets/fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "/assets/fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "/assets/img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "9d29f6c720d6dcf9c7e1c1097d8dba08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+];
